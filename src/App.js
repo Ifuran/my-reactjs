b7385e64ea62e3c543d7c544061d730c19bd7f7d
ifuran <silalhifran@gmail.com>
2023-08-26 12:33:22 +0700

    testing deploy to netlify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,14 +9,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <p>
+          <a className="App-link" href="https://www.instagram.com/ifurandesuu">
+            Saya
+          </a>{" "}
+          sedang mencoba library ReactJS
+        </p>
       </header>
     </div>
   );

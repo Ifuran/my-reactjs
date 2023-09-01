b5fd222725ba7ddb6a40273f8a6ba3b1d036b896
ifuran <silalahifran@gmail>
2023-09-01 20:01:06 +0700

    nambah alt tag img

--- src/Task/Portfolio/Hero.jsx
+++ src/Task/Portfolio/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
       <div className="container text-light">
         <h2>Full Stack Web Developer</h2>
         <p className="py-3">I code web, and I love what I do.</p>
-        <img className="my-3" src="https://mattfarley.ca/img/mf-avatar.svg" />
+        <img className="my-3" src="https://mattfarley.ca/img/mf-avatar.svg" alt="person" />
       </div>
     </div>
   );
